691724252558bb2488a82c8de782bce4525bf796
Hack Reactor Student <student@hackreactor.com>
2017-08-01 19:17:24 -0700

    passing 21 tests - student

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,45 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  Promise.resolve(req.cookies.shortlyid)
+  .then( (hash) => { 
+    //check if cookie has a session
+    if (!hash) {
+      //there is no session throw (catch and make a session)
+      throw hash;
+    } else {
+      //there is a session (return it to get authenticated)
+      return models.Sessions.get({ hash });
+    }
+  })
+  //authenticate the session
+  .then( (session) => { 
+    //if there is no session throw (catch and make a session)
+    if (!session) {
+      throw session;
+    }
+    return session;
+  })
+  //create a session
+  .catch( () => {
+    return models.Sessions.create()
+    .then( (result) => {
+      //get the id from the recently created session
+      //RAB// Why you ask? Don't ask questions. 
+      return models.Sessions.get({id: result.insertId});
+    })
+    //
+    .tap( (session) => {
+      console.log('***session**');
+      console.log(session);
+      res.cookie('shortlyid', session.hash);
+    });
+  })
+  .then( (session) => {  
+    req.session = session;
+    next();
+  });
+  
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  var header = req.headers.cookie;
+  
+  if (header) {
+    var cookies = header.split('; ');
+    cookies.forEach ( function (cookie) {
+      var crumbs = cookie.split('=');
+      var key = crumbs[0];
+      var value = crumbs[1];
+
+      req.cookies[key] = value;
+    });
+  } 
+  next();
 };
 
 module.exports = parseCookies;
